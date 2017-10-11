5089ba0e5152e4d5a2b86aed82fb99bbe8887091
FeDLviv <fed.lviv@gmail.com>
2017-10-11 15:52:21 +0300

    Create and add AngularJS filter - nullFilter

---
+++ js/filters/nullFilter.js
@@ -0,0 +1,9 @@
+myApp.filter('nullFilter', function () {
+    return function (str) {
+        if(str === null) {
+            return 'невідомо';
+        } else {
+            return str;
+        }
+    }
+});
